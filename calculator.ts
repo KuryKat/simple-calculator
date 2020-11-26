6ab0a27355445fa70fbc895452eebc11fade6977
KuryKat <57152799+KuryKat@users.noreply.github.com>
2020-11-26 15:39:57 -0300

    Create Calculator

---
+++ calculator.ts
@@ -0,0 +1,25 @@
+/**
+ * Calculate something
+ * @param num1 The first Number
+ * @param num2 The second Number
+ * @param type The operation type (Types accepted: "+", "=", "*", "/")
+ */
+export const calculator = (num1: number, num2: number, type: string): number => {
+    if (type === '+') return num1 + num2
+    else if (type === '-') return num1 - num2
+    else if (type === '*') return num1 * num2
+    else if (type === '/') {
+        if (num1 < 0) return num1 / num2
+        else if (num2 < 0) return num2 / num1
+        else return num1 / num2
+    }
+}
+
+const results: number[] = []
+
+results.push(calculator(2, 2, '+'))
+results.push(calculator(2, 2, '-'))
+results.push(calculator(2, 2, '*'))
+results.push(calculator(2, 2, '/'))
+
+console.log(results)
